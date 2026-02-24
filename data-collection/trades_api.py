3f1b00f4dd0b68e9fc439adb72fbdcd8e94bb824
ianchang8 <i.chang@smt.com>
2026-02-23 16:23:15 -0800

    gets trades for given slug

--- data-collection/trades_api.py
+++ data-collection/trades_api.py
@@ -1,7 +1,25 @@
 import requests
+import json
 
-url = "https://data-api.polymarket.com/trades?limit=100&takerOnly=true&market=0x3bed62b0b7e3eb52c1f0d8a5d11edad1f74989038fc1cae2889cdbe96a248dfe"
+event_slug = 'us-strikes-iran-by'
+event_url = f"https://gamma-api.polymarket.com/events/slug/{event_slug}"
 
-response = requests.get(url)
+response = requests.get(event_url)
 
-print(response.text)
+json_response = response.text
+
+data = json.loads(json_response)
+
+first_market = data["markets"][0]
+
+cID = first_market['conditionId']
+
+trades_url = f"https://data-api.polymarket.com/trades?limit=100&takerOnly=true&market={cID}"
+
+trades_response = requests.get(trades_url)
+
+json_trades = trades_response.json()
+
+prettify = json.dumps(json_trades, indent=2)
+
+print(prettify)
