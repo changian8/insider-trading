04e2b367655d9f9b990dc625e3c2c415b7f71606
ianchang8 <i.chang@smt.com>
2026-03-14 14:04:37 -0700

    updating to pass linter

--- data-collection/trades_api.py
+++ data-collection/trades_api.py
@@ -1,57 +1,72 @@
-import requests
+"""
+Gets the latest 1000 trades of a market for a given event slug
+then converts to a dataframe and writes it out to a csv
+"""
+
 import json
-import time
 
+import requests
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
-"""
-This function returns the latest 100 trades for a given event slug.
-Using the event slug it finds the condition ID then uses that to get all trades in that market
-The metadata returned includes user info, price, and timestamp 
-"""
 def get_trades(event_slug, offset, limit=1000):
-
+    """
+    This function returns the latest 1000 trades for a given event slug.
+    Using the event slug it finds the condition ID then uses that to get all trades in that market
+    The metadata returned includes user info, price, and timestamp.
+    Commented code represents manual condition id input,
+    which is used for slugs with multiple markets such us strikes iran or sb performer
+    """
     event_url = f"https://gamma-api.polymarket.com/events/slug/{event_slug}"
 
-    response = requests.get(event_url)
+    response = requests.get(event_url, timeout = 5)
 
     data = json.loads(response.text)
 
     first_market = data["markets"][0]
 
+    # Get custom market for lady gaga performing at superbowl
+    # Since the market who_performs_at_sb consists of multiple submarkets to bet on
     # first_market = next(
     #     m for m in data["markets"]
     #     if m.get("slug") == 'will-lady-gaga-perform-during-the-super-bowl-lx-halftime-show'
     # )
 
-    #cID = first_market['conditionId']
+    cond_id = first_market['conditionId']
 
-    cID = '0x3488f31e6449f9803f99a8b5dd232c7ad883637f1c86e6953305a2ef19c77f20'
+    # Manually input condition id for us_strikes_iran_by since there are multiple
+    # submarkets for each day that there are bets on
+    # cond_id = '0x3488f31e6449f9803f99a8b5dd232c7ad883637f1c86e6953305a2ef19c77f20'
 
-    trades_url = f"https://data-api.polymarket.com/trades?limit={limit}&takerOnly=true&side=BUY&offset={offset}&market={cID}"#&filterType=CASH&filterAmount=10"
+    trades_url = (f"https://data-api.polymarket.com/trades?limit={limit}&takerOnly=true&side=BUY&"
+                  f"offset={offset}&market={cond_id}")
 
-    trades_response = requests.get(trades_url)
+    trades_response = requests.get(trades_url, timeout = 5)
 
     json_trades = trades_response.json()
 
-    return(json_trades)
+    return json_trades
 
-event_slug = 'us-strikes-iran-by'
+# List of our different event slug markets we intend to get trades from
+EVENT_SLUG = 'us-strikes-iran-by'
 # 'us-strikes-iran-by'
 # 'will-there-be-a-streaker-at-super-bowl-lix'
 # 'who-will-perform-at-super-bowl-halftime-show'
 # 'us-strikes-iran-by-feb-28-odds-30-by-friday'
 # 'maduro-in-us-custody-by-january-31'
 # 'what-will-be-said-during-south-park-prediction-market-episode-tonight'
-all_trades1 = get_trades(event_slug, 0)
-all_trades2 = get_trades(event_slug, 1000)
-all_trades3 = get_trades(event_slug, 2000)
-all_trades4 = get_trades(event_slug, 3000)
-all_trades = all_trades1 + all_trades2 + all_trades3 + all_trades4
-#print(len(all_trades1))
 
+# Get latest 4000 trades from our market given SLUG
+# since max offset in API is 3000
+all_trades1 = get_trades(EVENT_SLUG, 0)
+all_trades2 = get_trades(EVENT_SLUG, 1000)
+all_trades3 = get_trades(EVENT_SLUG, 2000)
+all_trades4 = get_trades(EVENT_SLUG, 3000)
+all_trades = all_trades1 + all_trades2 + all_trades3 + all_trades4
+
+# Convert our trade data into a dataframe
+# selecting only the columns we are interested in
+# then creating column representing total value traded
+# and sorting our dataframe by timestamp to latest trades
 cols = [
     "proxyWallet",
     "side",
@@ -74,16 +89,5 @@ df = pd.DataFrame(
 df["total_trade_value"] = df["price"] * df["size"]
 df_sorted = df.sort_values(by="timestamp", ascending=False)
 
-print(df_sorted.head())
-print(df_sorted.shape)
-print(df_sorted.tail())
-print(df_sorted['price'])
-df_sorted["price"] = np.where(
-    df_sorted["outcomeIndex"] == 1,
-    1 - df_sorted["price"],
-    df_sorted["price"]
-)
-#print(df_sorted.duplicated().any())
-#df_sorted.to_csv("us_strikes_iran_trades_updated.csv", index=False)
-
-# Linter, Testing, Documentation, Virtual Environment (yaml/venv)
+# Convert dataframe to CSV
+# df_sorted.to_csv("us_strikes_iran_trades_updated.csv", index=False)
